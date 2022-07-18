71af5b001516c3d68ed0a279d6ff43e1cfc47330
Ageychev Denis <ageychevdenis@gmail.com>
2022-07-18 21:15:29 +0300

    Created a component with [alias]

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { withLayout } from "../layout/Layout";
 import axios from 'axios';
 import { MenuItem } from '../interfaces/menu.interfaces';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [state, setState] = useState<number>(4)
 
   return (
@@ -21,19 +21,30 @@ function Home(): JSX.Element {
       <Tag size="sizeM" color="green">green</Tag>
       <Tag size="sizeM" color="primary">primary</Tag>
       <Rating rating={state} isEditable setRating={setState}></Rating>
+<ul>
+				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+			</ul>
     </>
   )
 }
 
 export default withLayout(Home);
 
-export const getStaticProps: GetStaticProps = async () => {
-  const firstCategort = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-pages/find');
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
   return {
     props: {
       menu,
-      firstCategort
+      firstCategory
     }
   }
 }
+
+interface HomeProps extends Record<string, unknown>{
+  menu: MenuItem[];
+  firstCategory: number;
+}
+
