c3a25747a6aa4c899a2262b196552fb9a85ad343
Ageychev Denis <ageychevdenis@gmail.com>
2022-07-25 12:28:06 +0300

    Product page layout 1

---
+++ componetns/Card/Card.props.ts
@@ -0,0 +1,6 @@
+import { HtmlHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+
+export interface CardProps extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+   color?: 'white' | 'blue';
+   children: ReactNode;
+}
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,11 +1,26 @@
 import { TopPageComponentProps } from "./TopPageComponent.props";
-import styles from './Ptag.module.css';
+import styles from './TopPageComponent.module.css';
 import cn from 'classnames';
+import { Htag, Tag } from "../../componetns";
+import { HhData } from "../../componetns/HhData/HhData";
+import { TopLevelCategory } from "../../interfaces/page.interfaces";
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
    return (
-      <>
-         {products.length}
-      </>
+      <div className={styles.wrapper}>
+         <div className={styles.title}>
+            <Htag tag='h1'>{page.title}</Htag>
+            {products && <Tag color='grey' size="sizeM">{products.length}</Tag>}
+            <span>Сортировка</span>
+         </div>
+         <div>
+            {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+         </div>
+         <div className={styles.hhTitle}>
+            <Htag tag='h2'>Вакансии - {page.category}</Htag>
+            <Tag color='red' size="sizeM">hh.ru</Tag>
+         </div>
+         {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+      </div>
    )
 }
