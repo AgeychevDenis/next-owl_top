efe0cafe98a5db564dc37bd44ee4ef6db59e824c
Ageychev Denis <ageychevdenis@gmail.com>
2022-07-31 19:16:48 +0300

    Working with forms

--- componetns/Product/Product.tsx
+++ componetns/Product/Product.tsx
@@ -88,10 +88,10 @@ export const Product = ({ product, children, className, ...props }: ProductProps
             [styles.closed]: !isPeviewOpened
          })}>
             {product.reviews.map(r => (
-               <>
-                  <Review key={r._id} review={r} />
+               <div  key={r._id}>
+                  <Review review={r} />
                   <Divider />
-               </>
+               </div>
             ))}
             <ReviewForm productId={product._id} />
          </Card>
