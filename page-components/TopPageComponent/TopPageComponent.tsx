60a4ab911f793cf5ff9c99d9f82129d06db02d63
Ageychev Denis <ageychevdenis@gmail.com>
2022-08-04 22:39:59 +0300

    Fixing the useReducer bug

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -5,37 +5,41 @@ import { Advantages, Htag, Product, Ptag, Sort, Tag } from "../../componetns";
 import { HhData } from "../../componetns/HhData/HhData";
 import { TopLevelCategory } from "../../interfaces/page.interfaces";
 import { SortEnum } from "../../componetns/Sort/Sort.props";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { sortReducer } from "./sort.reducer";
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
-   const [{products: sortProducts, sort}, dispathSort] = useReducer(sortReducer, {products, sort: SortEnum.Rating});
+   const [{ products: sortProducts, sort }, dispathSort] = useReducer(sortReducer, { products, sort: SortEnum.Rating });
 
    const setSort = (sort: SortEnum) => {
-      dispathSort({type: sort});
+      dispathSort({ type: sort });
    }
+
+   useEffect(() => {
+      dispathSort({ type: 'reset', initialState: products });
+   }, [products])
 
    return (
       <div className={styles.wrapper}>
          <div className={styles.title}>
             <Htag tag='h1'>{page.title}</Htag>
             {products && <Tag color='grey' size="sizeM">{products.length}</Tag>}
-            <Sort sort={sort} setSort={setSort}/>
+            <Sort sort={sort} setSort={setSort} />
          </div>
          <div>
-            {sortProducts && sortProducts.map(p => (<Product key={p._id} product={p}/>))}
+            {sortProducts && sortProducts.map(p => (<Product key={p._id} product={p} />))}
          </div>
          <div className={styles.hhTitle}>
             <Htag tag='h2'>Вакансии - {page.category}</Htag>
             <Tag color='red' size="sizeM">hh.ru</Tag>
          </div>
-         {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>}
+         {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
          {page.advantages && page.advantages.length > 0 && <>
             <Htag tag='h2'>Преимущества</Htag>
-            <Advantages advantages={page.advantages}/>
+            <Advantages advantages={page.advantages} />
          </>
          }
-         {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText}}/>}
+         {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
          <Htag tag='h2'>Получаемые навыки</Htag>
          {page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
       </div>
