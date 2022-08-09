752778010f5e15a914d3178eb85a8de8616693b3
Ageychev Denis <ageychevdenis@gmail.com>
2022-08-09 15:19:45 +0300

    Adding meta tags to a page

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -8,13 +8,23 @@ import { ParsedUrlQuery } from 'querystring';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { TopPageComponent } from '../../page-components';
 import { API } from '../../helpers/api';
+import Head from 'next/head'
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-	return <TopPageComponent 
-	firstCategory={firstCategory}
-	page={page}
-	products={products}
-	/>
+	return <>
+		<Head>
+			<title>{page.metaTitle}</title>
+			<meta name='discription' content={page.metaDescription}/>
+			<meta property='og:title' content={page.metaTitle}/>
+			<meta property='og:discription' content={page.metaDescription}/>
+			<meta property='og:type' content="article"/>
+		</Head>
+		<TopPageComponent 
+			firstCategory={firstCategory}
+			page={page}
+			products={products}
+		/>
+	</> 
 }
 
 export default withLayout(TopPage);
